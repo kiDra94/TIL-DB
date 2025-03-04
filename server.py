0b59b847c468ee8374812c4689d766b0d9b450e7
Plavsic Drazen <drazen.plavsic@ads.bulme.intern>
2025-03-04 11:05:12 +0100

    added print to see if works

--- server.py
+++ server.py
@@ -4,5 +4,6 @@ HOST = "localhost"
 PORT = 8080
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server: #AF_INET - IPv4, SOCK_STREAM - TCP
-    server.bind(HOST, PORT)
+    server.bind((HOST, PORT))
     server.listen(1)
+    print(f"Server is listening to {HOST}:{PORT}")
