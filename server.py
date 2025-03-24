660760c03c21ebd3fd2f4f2a68b2c807c5c97adc
Drazen Plavsic <drazen.p@live.com>
2025-03-24 10:02:35 +0100

    endpoint implemente

--- server.py
+++ server.py
@@ -2,9 +2,10 @@ import socket #socket ist eine combination aus IP und PORT
 
 HOST = "localhost"
 PORT = 8080
+endpoint = (HOST, PORT)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server: #AF_INET - IPv4, SOCK_STREAM - TCP
-    server.bind((HOST, PORT))
+    server.bind((endpoint))
     server.listen(1)
     print(f"Server is listening to {HOST}:{PORT}")
 
