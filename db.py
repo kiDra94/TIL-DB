c3d952db4075f6627727191a32d15117cc294a5a
Drazen Plavsic <drazen.p@live.com>
2025-03-24 11:20:33 +0100

    transferd handling to the server

--- db.py
+++ db.py
@@ -13,21 +13,4 @@ def select(conn):
         for row in conn.cursor().execute("SELECT * FROM til;")
     ]
 pprint(select(CONNECTION))
-'''
-#server sollte so funktionieren!!!
-
-def handle():
-    if request.method == "GET":
-        # SELECT * FROM
-    elif request.method == "PUT":
-        # INSERT INTO
-    elif request.method == "POST":
-        # UPDATE .... SER
-    elif request.method == "DELETE":
-        # DELETE FROM
-while True:
-    request = conn.recive()
-    response = handle(request)
-    conn.send(response)
-'''
 CONNECTION.close()
